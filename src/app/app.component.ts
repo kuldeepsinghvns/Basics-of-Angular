090956f244be1d98cb51cbf434bed4db644b0d07
kuldeepsinghvns <133557298+kuldeepsinghvns@users.noreply.github.com>
2024-11-14 16:10:04 +0530

    Almost finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,19 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import { TestComponent } from './test/test.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HomeComponent,
-    TestComponent,
   ],
   template: `
     <main>
       <header class="brand-name">
-        <img  width="50px" class="brand-logo" src="/assets/sachin.jpeg" alt="logo" aria-hidden="true">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
       </header>
       <section class="content">
         <app-home></app-home>
-        <app-test></app-test>
       </section>
     </main>
   `,
@@ -25,10 +22,3 @@ import { TestComponent } from './test/test.component';
 export class AppComponent {
   title = 'homes';
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
