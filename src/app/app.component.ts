8ab49af281ceb72aa37824201a3093894780a1d0
kuldeepsinghvns <133557298+kuldeepsinghvns@users.noreply.github.com>
2024-11-14 14:38:37 +0530

    Hello World done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  // template: `<h1>Hello world!</h1>`,
-  styleUrl: './app.component.css'
+  imports: [
+    HomeComponent,
+  ],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img  width="50px" class="brand-logo" src="/assets/sachin.jpeg" alt="logo" aria-hidden="true">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Homes';
+  title = 'homes';
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
